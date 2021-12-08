3d6a244f1b33192de3b54fc6a6d26738a58b3cf8
Diogo Miyake <akio.miyake@live.com>
2021-12-08 12:18:31 -0300

    Mais itens da aula 2

--- golang/usecase/process_transaction/process_transaction.go
+++ golang/usecase/process_transaction/process_transaction.go
@@ -1,17 +1,19 @@
 package process_transaction
 
 import (
-	//"github.com/Miyake-Diogo/imersao-fullcicle-2021-gateway/domain/broker"
+	"github.com/Miyake-Diogo/imersao-fullcicle-2021-gateway/domain/broker"
 	"github.com/Miyake-Diogo/imersao-fullcicle-2021-gateway/domain/entity"
 	"github.com/Miyake-Diogo/imersao-fullcicle-2021-gateway/domain/repository"
 )
 
 type ProcessTransaction struct {
 	Repository repository.TransactionRepository
+	Producer   broker.ProducerInterface
+	Topic      string
 }
 
-func NewProcessTransaction(repository repository.TransactionRepository) *ProcessTransaction {
-	return &ProcessTransaction{Repository: repository}
+func NewProcessTransaction(repository repository.TransactionRepository, producerInterface broker.ProducerInterface, topic string) *ProcessTransaction {
+	return &ProcessTransaction{Repository: repository, Producer: producerInterface, Topic: topic}
 }
 
 func (p *ProcessTransaction) Execute(input TransactionDtoInput) (TransactionDtoOutput, error) {
@@ -19,21 +21,58 @@ func (p *ProcessTransaction) Execute(input TransactionDtoInput) (TransactionDtoO
 	transaction.ID = input.ID
 	transaction.AccountID = input.AccountID
 	transaction.Amount = input.Amount
-	_, invalidCC := entity.NewCreditCard(input.CreditCardNumber, input.CreditCardName, 
-		input.CreditCardExpirationMonth, input.CreditCardExpirationYear, input.CreditCardCVV)
+	cc, invalidCC := entity.NewCreditCard(input.CreditCardNumber, input.CreditCardName, input.CreditCardExpirationMonth, input.CreditCardExpirationYear, input.CreditCardCVV)
 	if invalidCC != nil {
-		err := p.Repository.Insert(transaction.ID, transaction.AccountID, 
-			transaction.Amount, entity.REJECTED, invalidCC.Error())
-		if err != nil {
-			return TransactionDtoOutput{}, err
-		}
-		output := TransactionDtoOutput{
-			ID: transaction.ID,
-			Status: entity.REJECTED,
-			ErrorMessage: invalidCC.Error(),
-		}
-		return output, nil
+		return p.rejectTransaction(transaction, invalidCC)
 	}
-	return TransactionDtoOutput{}, nil
+	transaction.SetCreditCard(*cc)
+	invalidTransaction := transaction.IsValid()
+	if invalidTransaction != nil {
+		return p.rejectTransaction(transaction, invalidTransaction)
+	}
 
+	return p.approveTransaction(input, transaction)
+
+}
+
+func (p *ProcessTransaction) approveTransaction(input TransactionDtoInput, transaction *entity.Transaction) (TransactionDtoOutput, error) {
+	err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, entity.APPROVED, "")
+	if err != nil {
+		return TransactionDtoOutput{}, err
+	}
+	output := TransactionDtoOutput{
+		ID:           transaction.ID,
+		Status:       entity.APPROVED,
+		ErrorMessage: "",
+	}
+	err = p.publish(output, []byte(transaction.ID))
+	if err != nil {
+		return TransactionDtoOutput{}, err
+	}
+	return output, nil
+}
+
+func (p *ProcessTransaction) rejectTransaction(transaction *entity.Transaction, invalidTransaction error) (TransactionDtoOutput, error) {
+	err := p.Repository.Insert(transaction.ID, transaction.AccountID, transaction.Amount, entity.REJECTED, invalidTransaction.Error())
+	if err != nil {
+		return TransactionDtoOutput{}, err
+	}
+	output := TransactionDtoOutput{
+		ID:           transaction.ID,
+		Status:       entity.REJECTED,
+		ErrorMessage: invalidTransaction.Error(),
+	}
+	err = p.publish(output, []byte(transaction.ID))
+	if err != nil {
+		return TransactionDtoOutput{}, err
+	}
+	return output, nil
+}
+
+func (p *ProcessTransaction) publish(output TransactionDtoOutput, key []byte) error {
+	err := p.Producer.Publish(output, key, p.Topic)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- golang/usecase/process_transaction/process_transaction_test.go
+++ golang/usecase/process_transaction/process_transaction_test.go
@@ -5,70 +5,109 @@ import (
 	"time"
 	"github.com/golang/mock/gomock"
 	"github.com/Miyake-Diogo/imersao-fullcicle-2021-gateway/domain/entity"
+	mock_broker "github.com/Miyake-Diogo/imersao-fullcicle-2021-gateway/adapter/broker/mock"
 	mock_repository "github.com/Miyake-Diogo/imersao-fullcicle-2021-gateway/domain/repository/mock"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestProcessTransaction_ExecuteInvalidCreditCard(t *testing.T) {
 	input := TransactionDtoInput{
-		ID:		  "1",
-		AccountID: "1",
-		CreditCardNumber: "1234567890123456",
-		CreditCardName: "John Doe",
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "40000000000000000",
+		CreditCardName:            "Tião Carneiro",
 		CreditCardExpirationMonth: 12,
-		CreditCardExpirationYear: time.Now().Year() + 1,
-		CreditCardCVV: 123,
-		Amount: 145.50,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardCVV:             123,
+		Amount:                    200,
 	}
 	expectedOutput := TransactionDtoOutput{
-		ID:		  "1",
-		Status: entity.REJECTED,
+		ID:           "1",
+		Status:       entity.REJECTED,
 		ErrorMessage: "invalid credit card number",
-
 	}
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	repositoryMock := mock_repository.NewMockTransactionRepository(ctrl)
-	repositoryMock.
-		EXPECT().
+	repositoryMock.EXPECT().
 		Insert(input.ID, input.AccountID, input.Amount, expectedOutput.Status, expectedOutput.ErrorMessage).
 		Return(nil)
 
-	usecase := NewProcessTransaction(repositoryMock)
-	output , err := usecase.Execute(input)
-	assert.Equal(t, expectedOutput, output)
+	producerMock := mock_broker.NewMockProducerInterface(ctrl)
+	producerMock.EXPECT().Publish(expectedOutput, []byte(input.ID), "transactions_result")
+
+	usecase := NewProcessTransaction(repositoryMock, producerMock, "transactions_result")
+	output, err := usecase.Execute(input)
 	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
 }
 
-func TestProcessTransaction_ExecuteRejectTransaction(t *testing.T) {
+func TestProcessTransaction_ExecuteRejectedTransaction(t *testing.T) {
 	input := TransactionDtoInput{
-		ID:		  "1",
-		AccountID: "1",
-		CreditCardNumber: "5355119872960768",
-		CreditCardName: "John Doe",
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "5355119872960768",
+		CreditCardName:            "Tião Carneiro",
 		CreditCardExpirationMonth: 12,
-		CreditCardExpirationYear: time.Now().Year() + 1,
-		CreditCardCVV: 123,
-		Amount: 4145.50,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardCVV:             123,
+		Amount:                    1200,
 	}
 	expectedOutput := TransactionDtoOutput{
-		ID:		  "1",
-		Status: entity.REJECTED,
-		ErrorMessage: "Invalid credit card number",
-
+		ID:           "1",
+		Status:       entity.REJECTED,
+		ErrorMessage: "you dont have limit for this transaction",
 	}
-    ctrl := gomock.NewController(t)
+	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
 	repositoryMock := mock_repository.NewMockTransactionRepository(ctrl)
-	repositoryMock.
-		EXPECT().
+	repositoryMock.EXPECT().
 		Insert(input.ID, input.AccountID, input.Amount, expectedOutput.Status, expectedOutput.ErrorMessage).
 		Return(nil)
 
-	usecase := NewProcessTransaction(repositoryMock)
-	output , err := usecase.Execute(input)
-	assert.Equal(t, expectedOutput, output)
+	producerMock := mock_broker.NewMockProducerInterface(ctrl)
+	producerMock.EXPECT().Publish(expectedOutput, []byte(input.ID), "transactions_result")
+
+	usecase := NewProcessTransaction(repositoryMock, producerMock, "transactions_result")
+
+	output, err := usecase.Execute(input)
 	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+
+}
+
+func TestProcessTransaction_ExecuteApprovedTransaction(t *testing.T) {
+	input := TransactionDtoInput{
+		ID:                        "1",
+		AccountID:                 "1",
+		CreditCardNumber:          "5355119872960768",
+		CreditCardName:            "Tião Carneiro",
+		CreditCardExpirationMonth: 12,
+		CreditCardExpirationYear:  time.Now().Year(),
+		CreditCardCVV:             123,
+		Amount:                    900,
+	}
+	expectedOutput := TransactionDtoOutput{
+		ID:           "1",
+		Status:       entity.APPROVED,
+		ErrorMessage: "",
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repositoryMock := mock_repository.NewMockTransactionRepository(ctrl)
+	repositoryMock.EXPECT().
+		Insert(input.ID, input.AccountID, input.Amount, expectedOutput.Status, expectedOutput.ErrorMessage).
+		Return(nil)
+
+	producerMock := mock_broker.NewMockProducerInterface(ctrl)
+	producerMock.EXPECT().Publish(expectedOutput, []byte(input.ID), "transactions_result")
+
+	usecase := NewProcessTransaction(repositoryMock, producerMock, "transactions_result")
+	output, err := usecase.Execute(input)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedOutput, output)
+
 }
